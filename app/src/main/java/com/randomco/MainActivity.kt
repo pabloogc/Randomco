e99c3387db7f22ce74214b5d92611293467ccbdf
Pablo Orgaz <pabloogc@gmail.com>
2018-04-20 16:31:36 +0200

    Empty project

---
+++ app/src/main/java/com/randomco/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.randomco
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.randomco.randomco.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
